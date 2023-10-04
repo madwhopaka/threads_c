97882b9de5bcb38f8eeadd5cceee0e132745a1f9
madhu paka <madhupaka11@gmail.com>
2023-10-04 12:22:34 +0530

    making left side bar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "64f08dbe71e3ca32"; }
+/******/ 		__webpack_require__.h = function() { return "83e8509e107258fa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,17 +1,24 @@
+"use client";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 import { sidebarLinks } from "@/constants";
 import Image from "next/image";
 
 function LeftSidebar() {
+  const router = useRouter();
+  const pathName = usePathname();
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-4 px-6">
         {sidebarLinks.map((item, key) => {
+          const isActive =
+            (pathName.includes(item.route) && item.route.length > 1) ||
+            pathName === item.route;
           return (
             <Link
               href={item.route}
               key={item.label}
-              className="leftsidebar_link"
+              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
             >
               <Image
                 src={item.imgURL}
